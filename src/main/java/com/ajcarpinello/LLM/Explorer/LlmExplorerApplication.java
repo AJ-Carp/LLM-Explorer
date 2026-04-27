144f3ba874cf116141d24ddc6e992e659bdf2f86
AJ Carpinello <ajcarp81003@gmail.com>
2026-04-26 22:18:50 -0400

    first commit

---
+++ src/main/java/com/ajcarpinello/LLM/Explorer/LlmExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.ajcarpinello.LLM.Explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajcarpinello/LLM/Explorer/LlmExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajcarpinello.LLM.Explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
